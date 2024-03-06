a02efffd7f013ca304b48419da1775db908e798c
OL <oiva.laitinen2@gmail.com>
2024-03-06 21:24:46 +0200

    Update config.fish to not be symlink

--- config.fish
+++ config.fish
@@ -1,1 +1,15 @@
-/home/user/.dotfiles/config.fish
+if status is-interactive
+    # not fish compliant
+    # [ -f "/home/user/.ghcup/env" ] && source "/home/user/.ghcup/env" # ghcup-env
+
+    if command -q tmux && [ "$TERM" != "tmux" ] && [ -z "$TMUX" ]
+		tmux attach
+    end
+
+    if command -q fd
+		export FZF_DEFAULT_COMMAND='fd -H -E .git .'
+    end
+
+
+    fish_add_path $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin
+end
