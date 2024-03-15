6c8a935ab910987ba9e6cc9cd721f1c37f5b79a7
OL <oiva.laitinen2@gmail.com>
2024-03-15 21:13:28 +0200

    Add nvm default version if it doesnt exist

--- config.fish
+++ config.fish
@@ -12,6 +12,8 @@ if status is-interactive
 
 	set -gx fish_greeting 
 	set -gx EDITOR nvim
+	set -gx VISUAL nvim
 	set -gx PAGER "nvim +Man!"
+	set -q nvm_default_version; or set -U nvm_default_version latest
     fish_add_path $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin $HOME/.cabal/bin $HOME/.ghcup/bin
 end
