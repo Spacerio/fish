b139c4848c99f2465bc595cc0639a3512b5c73eb
OL <oiva.laitinen2@gmail.com>
2024-03-22 22:39:40 +0200

    Remove alias for alacritty

--- functions/alacritty.fish
+++
@@ -1,4 +0,0 @@
-function alacritty --wraps='cd /mnt/c/Users/Omistaja/AppData/Roaming/alacritty' --description 'alias alacritty cd /mnt/c/Users/Omistaja/AppData/Roaming/alacritty'
-  cd /mnt/c/Users/Omistaja/AppData/Roaming/alacritty $argv
-        
-end
